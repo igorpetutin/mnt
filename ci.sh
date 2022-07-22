8c21f225458161f2cc4bb65f5c4af185c6494895
Igor Petutin <igor@petutin.com>
2022-07-22 12:15:55 +0200

    1

--- ci.sh
+++ ci.sh
@@ -4,7 +4,7 @@ aws ecs register-task-definition --cli-input-json file://./cadvisor-node-exporte
 aws ecs register-task-definition --cli-input-json file://./prometheus-grafana-definition.json --region us-west-2
 
 #2
-aws ecs create-service --cluster MyWorkingCluster --service-name cadvisor-node-exporter --task-definition cadvisor-node-exporter-definition:1 --launch-type EC2 --scheduling-strategy DAEMON --region us-west-2
+aws ecs create-service --cluster MonitoringECSTest --service-name cadvisor-node-exporter --task-definition cadvisor-node-exporter-definition:1 --launch-type EC2 --scheduling-strategy DAEMON --region us-west-2
 
 #3
-aws ecs run-task --cluster MyWorkingCluster --task-definition prometheus-grafana-definition:1  --region us-west-2
+aws ecs run-task --cluster MonitoringECSTest --task-definition prometheus-grafana-definition:1  --region us-west-2
